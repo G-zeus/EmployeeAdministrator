daf983b2f2de735c2b19e8fab1e9653f6e50dade
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-07-03 14:40:29 -0600

    feat(Repositories):Create user functions

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -19,9 +21,12 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    public function __construct(ManagerRegistry $registry)
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(ManagerRegistry $registry, UserPasswordHasherInterface $userPasswordHasher)
     {
         parent::__construct($registry, User::class);
+        $this->userPasswordHasher = $userPasswordHasher;
     }
 
     public function save(User $entity, bool $flush = false): void
@@ -55,6 +60,96 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 
         $this->save($user, true);
     }
+
+    public function create(string $email, string $name, string$userName, string $password, array $roles): array
+    {
+        try {
+
+            $user = new User();
+            $encodePassword = $this->userPasswordHasher->hashPassword($user, $password);
+            $user->setUserName($userName);
+            $user->setName($name);
+            $user->setEmail($email);
+            $user->setPassword($encodePassword);
+            $user->setRoles($roles);
+
+            $this->_em->persist($user);
+            $this->_em->flush();
+
+
+            return [
+                'success' => true,
+                'user' => $user,
+                'msg' => "Administrador creado exitosamente"
+            ];
+
+
+        } catch (\Exception $exception) {
+
+            return [
+                'success' => false,
+                'msg' => $exception->getMessage()
+            ];
+        }
+
+    }
+
+    public function userList(int $page = 1, int $limit = 10):array{
+
+        $query = $this->createQueryBuilder('user')
+//            ->where('')
+            ->setFirstResult($limit * ($page - 1)) // Offset
+            ->setMaxResults($limit)
+            ->orderBy('user.id', 'DESC');
+
+        $paginator = new Paginator($query->getQuery());
+
+        $totalUsers = count($paginator);
+        $totalPages = ceil($totalUsers / $limit);
+        $resultUsers = $paginator->getQuery()->getResult();
+
+        return array(
+            "items" => $resultUsers,
+            "countItems" => count($resultUsers),
+            "totalItems" => $totalUsers,
+            "currentPage" => $page,
+            "totalPages" => intval($totalPages)
+        );
+
+    }
+
+    public function createAdmin(): array
+    {
+        try {
+            $admin = $this->findOneBy(['email' => 'admin@app.master.com']);
+
+            if (!empty($admin)) {
+                $this->upgradePassword($admin, $this->userPasswordHasher->hashPassword($admin, 'test'));
+
+                return [
+                    'success' => true,
+                    'msg' => "Password del usuario 'admin' reseteada exitosamente"
+                ];
+            }
+
+            $this->create('admin@app.master.com', 'admin', 'admin' , 'test', ['COMPANY']);
+
+            return [
+                'success' => true,
+                'msg' => "Usuario 'admin' creado exitosamente"
+            ];
+
+        } catch (\Exception $exception) {
+
+            return [
+                'success' => false,
+                'msg' => $exception->getMessage()
+            ];
+        }
+
+
+    }
+
 
 //    /**
 //     * @return User[] Returns an array of User objects
